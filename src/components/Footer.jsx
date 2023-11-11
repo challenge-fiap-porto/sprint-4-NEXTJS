4386ee0e1b16acdd70e1771900f3f868bb93a0e0
dogzeira <douglasdanilobutu13@gmail.com>
2023-11-11 17:14:03 -0300

    Correção do footer do projeto e alteração no favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,7 @@ import youtube from "../imgs/youtube.png";
 import twitter from "../imgs/twitter.png";
 import tiktok from "../imgs/tiktok.png";
 import facebook from "../imgs/facebook.png";
+import portinho2 from "../imgs/portinho2.png";
 
 function Footer() {
   return (
@@ -17,12 +18,8 @@ function Footer() {
           <h2 className="ajuste-espaços">Links Rápidos</h2>
           <li>Perguntas Frequentes</li>
           <li>SAC e Telefones</li>
-          <li> WhatsApp</li>
+          <li>WhatsApp</li>
           <li>Endereços</li>
-          <li>Ouvidoria</li>
-          <li>Canal de Denúncia</li>
-          <li>Encontre um Corretor</li>
-          <li>Chat Online</li>
         </ul>
 
         <ul>
@@ -31,10 +28,6 @@ function Footer() {
           <li>Nossa História</li>
           <li>Notícias e imprensa</li>
           <li>Sustentabilidade</li>
-          <li>Instituto Porto Seguro</li>
-          <li>Blog</li>
-          <li>Patrocínios</li>
-          <li>Dicas de Segurança</li>
         </ul>
         <div className="div-adjustment">
           <Image
@@ -44,28 +37,32 @@ function Footer() {
           />
           <h2>Acompanhe a porto nas redes sociais:</h2>
 
-<div className="espaço-icones">
-          <a href="https://www.facebook.com/porto" target="_blank">
-            <Image className="icon-footer" src={facebook} alt="facebook" />
-          </a>
-          <a href="https://www.instagram.com/porto" target="_blank">
-            <Image className="icon-footer" src={insta} alt="instagram" />
-          </a>
-          <a href="https://www.linkedin.com/company/porto/" target="_blank">
-            <Image className="icon-footer" src={linkedin} alt="linkedin" />
-          </a>
-          <a href="https://www.youtube.com/portoseguro" target="_blank">
-            <Image className="icon-footer" src={youtube} alt="youtube" />
-          </a>
-          <a href="https://twitter.com/portoseguro" target="_blank">
-            <Image className="icon-footer" src={twitter} alt="twitter" />
-          </a>
-          <a href="https://www.tiktok.com/@portoseguro" target="_blank">
-            <Image className="icon-footer" src={tiktok} alt="tiktok" />
-          </a>
+          <div className="espaço-icones">
+            <a href="https://www.facebook.com/porto" target="_blank">
+              <Image className="icon-footer" src={facebook} alt="facebook" />
+            </a>
+            <a href="https://www.instagram.com/porto" target="_blank">
+              <Image className="icon-footer" src={insta} alt="instagram" />
+            </a>
+            <a href="https://www.linkedin.com/company/porto/" target="_blank">
+              <Image className="icon-footer" src={linkedin} alt="linkedin" />
+            </a>
+            <a href="https://www.youtube.com/portoseguro" target="_blank">
+              <Image className="icon-footer" src={youtube} alt="youtube" />
+            </a>
+            <a href="https://twitter.com/portoseguro" target="_blank">
+              <Image className="icon-footer" src={twitter} alt="twitter" />
+            </a>
+            <a href="https://www.tiktok.com/@portoseguro" target="_blank">
+              <Image className="icon-footer" src={tiktok} alt="tiktok" />
+            </a>
           </div>
         </div>
       </div>
+        <div className="ajuste-portinho2">
+          <Image className="portinho2" src={portinho2} />
+        </div>
+      
     </footer>
   );
 }
